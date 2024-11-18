673b25e27489f6fd6560db18a9fd4a58898289a0
Takib Yeasar <mdtakibyeasar@gmail.com>
2024-11-18 10:57:34 +0600

    updated

--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newapp',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# CELERY_BROKER_URL = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
+# celery settings
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_Backend', 'redis://redis:6379/0')
 
